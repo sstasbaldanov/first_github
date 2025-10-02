08e24795388c1952b2944159a4643b29c5a7c770
sstas <sstasbaldanov@gmail.com>
2025-10-02 17:17:02 +0700

    feat: Add about page

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
 ]
+
--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+
+def about_view(request):
+    return HttpResponse("<h1>Об авторе</h1>")
+
