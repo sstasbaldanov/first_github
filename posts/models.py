27d2f2db733bc6419c45751f7b3dfbbbd38a8b02
sstas <sstasbaldanov@gmail.com>
2025-10-15 20:04:43 +0700

    feat: add author

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    author = models.CharField(max_length=100, default='Anonymous')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
